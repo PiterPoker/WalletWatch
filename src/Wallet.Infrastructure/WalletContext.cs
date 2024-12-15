9757841886014a7de221b9794267777a1d3655ff
PiterPoker <tema5286@gmail.com>
2024-12-15 11:58:59 +0300

    init: Initialized project Wallet.Infrastructure

---
+++ src/Wallet.Infrastructure/WalletContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Domain.SeedWork;
+
+namespace Wallet.Infrastructure
+{
+    public class WalletContext : DbContext, IUnitOfWork
+    {
+        public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
