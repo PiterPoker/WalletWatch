1fe2d892610d4cc0ad49293c3bb1bf4e71b53fc2
PiterPoker <tema5286@gmail.com>
2024-12-15 21:55:40 +0300

    feat: Added WalletContext

--- src/Wallet.Infrastructure/GlobalUsings.cs
+++ src/Wallet.Infrastructure/GlobalUsings.cs
@@ -4,3 +4,6 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Design;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.EntityFrameworkCore.Storage;
+global using Wallet.Domain.AggregatesModel.AccountAggregate;
+global using WalletAggregate = Wallet.Domain.AggregatesModel.WalletAggregate;
+global using Wallet.Domain.SeedWork;
--- src/Wallet.Infrastructure/WalletContext.cs
+++ src/Wallet.Infrastructure/WalletContext.cs
@@ -1,17 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Wallet.Domain.SeedWork;
-
-namespace Wallet.Infrastructure
+﻿namespace Wallet.Infrastructure
 {
     public class WalletContext : DbContext, IUnitOfWork
     {
-        public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<WalletAggregate.Wallet> Wallets { get; set; }
+        public DbSet<WalletAggregate.Family> Families { get; set; }
+        public DbSet<WalletAggregate.Member> Members { get; set; }
+        public DbSet<WalletAggregate.Currency> Currencies { get; set; }
+
+
+        private readonly IMediator _mediator;
+        private IDbContextTransaction _currentTransaction;
+
+
+        public WalletContext(DbContextOptions<WalletContext> options) : base(options) { }
+
+        public IDbContextTransaction GetCurrentTransaction() => _currentTransaction;
+
+        public bool HasActiveTransaction => _currentTransaction != null;
+
+        public WalletContext(DbContextOptions<WalletContext> options, IMediator mediator) : base(options)
         {
-            throw new NotImplementedException();
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+
+
+            System.Diagnostics.Debug.WriteLine("WalletContext::ctor ->" + this.GetHashCode());
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            // Dispatch Domain Events collection. 
+            // Choices:
+            // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
+            // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
+            // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
+            // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
+            await _mediator.DispatchDomainEventsAsync(this);
+
+            // After executing this line all the changes (from the Command Handler and Domain Event Handlers) 
+            // performed through the DbContext will be committed
+            _ = await base.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            if (_currentTransaction != null) return null;
+
+            _currentTransaction = await Database.BeginTransactionAsync(IsolationLevel.ReadCommitted);
+
+            return _currentTransaction;
+        }
+
+        public async Task CommitTransactionAsync(IDbContextTransaction transaction)
+        {
+            if (transaction == null) 
+                throw new ArgumentNullException(nameof(transaction));
+            if (transaction != _currentTransaction) 
+                throw new InvalidOperationException($"Transaction {transaction.TransactionId} is not current");
+
+            try
+            {
+                await SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (HasActiveTransaction)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (HasActiveTransaction)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
         }
     }
 }
