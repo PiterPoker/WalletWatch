3fabd3acb7ee130cb0c4d69b219860209183730e
PiterPoker <tema5286@gmail.com>
2025-05-05 19:54:28 +0300

    feat: Added Cors

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -3,6 +3,16 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "gatewayAllowOriginsCors", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader(); ;
+    });
+});
+
 builder.Configuration
       .SetBasePath(builder.Environment.ContentRootPath)
       .AddOcelot();
@@ -10,5 +20,6 @@ builder.Services
     .AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+app.UseCors("gatewayAllowOriginsCors");
 await app.UseOcelot();
 await app.RunAsync();
